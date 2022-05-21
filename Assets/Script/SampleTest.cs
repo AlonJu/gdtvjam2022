f6f5da23de192026c0591678c9ea7ee157e4bf49
Gnwel <coolthr@gmx.com>
2022-05-21 04:48:14 -0500

    Test

--- Assets/Script/SampleTest.cs
+++ Assets/Script/SampleTest.cs
@@ -2,21 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public GameObject _gameObject;
+
 
 
 
 public class SampleTest : MonoBehaviour
 {
+    public GameObject _gameJbject;
+    public Vector3 pos;
     // Start is called before the first frame update
     void Start()
     {
-    
+     _gameJbject=GetComponent<GameObject>();
+     
+     
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        pos = _gameJbject.transform.position;
+        Debug.Log(pos);
     }
 }
